8f451f57c737f846e8d1e87b764f77269e61b6df
vaibhav <vaibhavsaraf.08@gmail.com>
2024-02-17 00:41:31 +0530

    navigation

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -16,8 +16,22 @@ const Dashboard: React.FC<Props> = () => {
 
       <div className="h-[107px] body-box p-[60px]">
         <p className='text-skin-base font-[700] text-[32px]'>Choose your new site</p>
-        <div className=''>
-          
+        <div className='flex justify-center items-center'>
+          {
+            [1,2,3].map(stage => (
+              <div key={stage} className="flex flex-col w-[29px]">
+                <div className='flex justify-between items-center w-[20px]'>
+                  {
+                    (stage <=1) && (
+                      <div className='rounded-full border-black w-[12px] h-[12px] bg-black'></div>
+                    )
+                  }
+                  <p className='font-[500] text-[18px] text-skin-base'>{stage}</p>
+                </div>
+                <div className='h-[2.5px] bg-black'></div>
+              </div>
+            ))
+          }
         </div>
       </div>
     </div>
