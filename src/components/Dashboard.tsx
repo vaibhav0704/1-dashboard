fb7f6b235f3d1d48606051792d10c09da5a4b720
vaibhav <vaibhavsaraf.08@gmail.com>
2024-02-17 03:17:08 +0530

    improved status styles

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -19,19 +19,38 @@ const Dashboard: React.FC<Props> = () => {
         <div className='flex justify-center items-center'>
           {
             [1,2,3].map(stage => (
-              <div key={stage} className="flex flex-col w-[29px]">
-                <div className='flex justify-between items-center w-[20px]'>
-                  {
-                    (stage <=1) && (
-                      <div className='rounded-full border-black w-[12px] h-[12px] bg-black'></div>
-                    )
-                  }
-                  <p className='font-[500] text-[18px] text-skin-base'>{stage}</p>
+              <>
+                <div key={stage} className="flex flex-col w-[29px]">
+                  <div className='flex justify-around items-center w-[20px]'>
+                    {
+                      (stage <=1) && (
+                        <div className='rounded-full border-black w-[12px] h-[12px] bg-black'>
+                        </div>
+                      )
+                    }
+                    <p className='font-[500] text-[18px] text-skin-base'>{stage}</p>
+                  </div>
+                  <div className='h-[2.5px] bg-black'></div>
                 </div>
-                <div className='h-[2.5px] bg-black'></div>
-              </div>
+                {
+                  stage < 3 && (
+                    <>
+                      <div className="w-[76px] border-[0.25px] border-dashed"></div>
+                      <div className="w-0 h-0 border-l-[3px] border-l-black border-t-[3px] border-t-transparent border-b-[3px] border-b-transparent"></div>
+                    </>
+
+                  )
+                }              
+              </>
             ))
           }
+        </div>
+      </div>
+
+      {/* main body area */}
+      <div className="min-h-full body-box p-[60px]">
+        <div className="">
+
         </div>
       </div>
     </div>
